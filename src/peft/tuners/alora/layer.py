f9280f95555d3e849e3c8e386fe169bb06cbe0f3
Alejandro Rodriguez Salamanca <alexrs95@gmail.com>
2023-10-05 11:26:18 +0200

    use einsum

--- src/peft/tuners/alora/layer.py
+++ src/peft/tuners/alora/layer.py
@@ -177,30 +177,6 @@ class Linear(nn.Linear, AloraLayer):
     def _linear(self, input: torch.Tensor) -> torch.Tensor:
         return F.linear(input, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        previous_dtype = x.dtype
-
-        if self.disable_adapters:
-            if self.merged:
-                self.unmerge()
-            result = self._linear(x)
-        elif self.merged:
-            result = self._linear(x)
-        else:
-            result = self._linear(x)
-            for active_adapter in self.active_adapters:
-                if active_adapter not in self.lora_A.keys():
-                    continue
-                lora_A = self.lora_A[active_adapter]
-                lora_B = self.lora_B[active_adapter]
-                dropout = self.lora_dropout[active_adapter]
-                scaling = self.scaling[active_adapter]
-                x = x.to(lora_A.weight.dtype)
-                result += lora_B(lora_A(dropout(x))) * scaling
-
-        result = result.to(previous_dtype)
-        return result
-
     # def forward(self, x: torch.Tensor) -> torch.Tensor:
     #     previous_dtype = x.dtype
 
@@ -219,14 +195,38 @@ class Linear(nn.Linear, AloraLayer):
     #             lora_B = self.lora_B[active_adapter]
     #             dropout = self.lora_dropout[active_adapter]
     #             scaling = self.scaling[active_adapter]
-
-    #             # Using einsum for lora_A matrix multiplication
-    #             x_transformed = torch.einsum('bi,ij->bj', x, lora_A.weight)
-    #             x_transformed = dropout(x_transformed)
-
-    #             # Using einsum for lora_B matrix multiplication
-    #             result += torch.einsum('bj,jk->bk', x_transformed, lora_B.weight) * scaling
+    #             x = x.to(lora_A.weight.dtype)
+    #             result += lora_B(lora_A(dropout(x))) * scaling
 
     #     result = result.to(previous_dtype)
     #     return result
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        previous_dtype = x.dtype
+
+        if self.disable_adapters:
+            if self.merged:
+                self.unmerge()
+            result = self._linear(x)
+        elif self.merged:
+            result = self._linear(x)
+        else:
+            result = self._linear(x)
+            for active_adapter in self.active_adapters:
+                if active_adapter not in self.lora_A.keys():
+                    continue
+                lora_A = self.lora_A[active_adapter]
+                lora_B = self.lora_B[active_adapter]
+                dropout = self.lora_dropout[active_adapter]
+                scaling = self.scaling[active_adapter]
+
+                # Using einsum for lora_A matrix multiplication
+                x_transformed = torch.einsum('bi,ij->bj', x, lora_A.weight)
+                x_transformed = dropout(x_transformed)
+
+                # Using einsum for lora_B matrix multiplication
+                result += torch.einsum('bj,jk->bk', x_transformed, lora_B.weight) * scaling
+
+        result = result.to(previous_dtype)
+        return result
 
