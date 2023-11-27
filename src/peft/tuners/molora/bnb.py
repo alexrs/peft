aff6d5e77997f204fed4f3ea714302663504e284
Alejandro Rodriguez Salamanca <alexrs95@gmail.com>
2023-11-27 22:45:38 +0100

    debug

--- src/peft/tuners/molora/bnb.py
+++ src/peft/tuners/molora/bnb.py
@@ -252,6 +252,8 @@ if is_bnb_4bit_available():
                 # assume we do not use value
                 # output = lora_router(x, bax)
                 expert_weights = lora_router(x, bax)
+                print(expert_weights.shape)
+                print(bax.shape)
                 output = torch.einsum("...e,...ed->...d", expert_weights, bax)
                 # output = torch.einsum('bsn,bsne->bse', attention, bax)  # [batch_si
 
